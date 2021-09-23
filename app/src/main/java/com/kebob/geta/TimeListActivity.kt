babf3885487d2b1069607f6f9fc16ebd96220234
Hyesung <comet_0419@naver.com>
2021-09-23 14:21:00 +0900

    feat: Add back button on action bar

--- app/src/main/java/com/kebob/geta/TimeListActivity.kt
+++ app/src/main/java/com/kebob/geta/TimeListActivity.kt
@@ -2,6 +2,7 @@ package com.kebob.geta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.kebob.geta.databinding.ActivityTimeListBinding
 
 class TimeListActivity : AppCompatActivity() {
@@ -13,9 +14,21 @@ class TimeListActivity : AppCompatActivity() {
         binding = ActivityTimeListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.title = "급식 시간 추가"
+        }
+
         val fabAdd = binding.fabAdd
         fabAdd.setOnClickListener {
             TODO("startActivity 등록 폼")
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
