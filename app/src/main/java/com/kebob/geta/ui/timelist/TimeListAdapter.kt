36d99420aedab3237e0b348b9b759544186f2498
PsPLoG <gkgkgkrj@gmail.com>
2021-09-24 05:36:23 +0900

    feat: TimeListAdapter 디자인 수정, 테마색상 적용

--- app/src/main/java/com/kebob/geta/ui/timelist/TimeListAdapter.kt
+++ app/src/main/java/com/kebob/geta/ui/timelist/TimeListAdapter.kt
@@ -6,22 +6,35 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.kebob.geta.data.Meal
 import com.kebob.geta.R
+import com.kebob.geta.data.Meal
+import com.kebob.geta.databinding.ItemRegisteredMealListBinding
+import com.kebob.geta.ui.RegisterActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TimeListAdapter(var dataSet: List<Meal>) : RecyclerView.Adapter<TimeListAdapter.ViewHolder>() {
-    class ViewHolder(view: View, listener: OnItemLongClickListener) : RecyclerView.ViewHolder(view) {
-        val tvMeal: TextView = view.findViewById(R.id.tv_meal)
-        val tvTime: TextView = view.findViewById(R.id.tv_time_range)
+    class ViewHolder(private val binding: ItemRegisteredMealListBinding, listener: OnItemLongClickListener) : RecyclerView.ViewHolder(binding.root) {
 
         init {
-            view.setOnLongClickListener {
+            itemView.setOnLongClickListener {
                 val position = adapterPosition
                 if (position != RecyclerView.NO_POSITION) {
-                    listener.onItemLongClick(view, position)
+                    listener.onItemLongClick(itemView, position)
                 }
 
                 false
+            }
+        }
+
+        fun bind(meal: Meal) {
+            binding.tvMeal.text = meal.mealName
+            binding.tvTimeRange.text = "${meal.startTime} ~ ${meal.endTime}"
+
+            binding.textView2.text = when (meal.mealType) {
+                RegisterActivity.MealType.Meal.name -> itemView.context.getString(R.string.common_meal)
+                RegisterActivity.MealType.Snack.name -> itemView.context.getString(R.string.common_snack)
+                else -> ""
             }
         }
     }
@@ -33,16 +46,13 @@ class TimeListAdapter(var dataSet: List<Meal>) : RecyclerView.Adapter<TimeListAd
     private lateinit var onItemLongClickListener: OnItemLongClickListener
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.item_registered_meal_list, viewGroup, false)
-
+        val view = ItemRegisteredMealListBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
         return ViewHolder(view, onItemLongClickListener)
     }
 
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.tvMeal.text = dataSet[position].mealName
-        holder.tvTime.text = "${dataSet[position].startTime} ~ ${dataSet[position].endTime}"
+        holder.bind(dataSet[position])
     }
 
     override fun getItemCount() = dataSet.size
