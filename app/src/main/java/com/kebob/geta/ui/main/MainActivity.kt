84b612ececa1f2b20c89d933a9e58929c851467a
good797979 <good79797979@naver.com>
2021-09-24 11:12:39 +0900

    feat: main activity dialog 추가

--- app/src/main/java/com/kebob/geta/ui/main/MainActivity.kt
+++ app/src/main/java/com/kebob/geta/ui/main/MainActivity.kt
@@ -9,8 +9,10 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.CheckBox
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.google.firebase.database.ktx.database
@@ -23,10 +25,11 @@ import com.kebob.geta.data.MealRequest
 import com.kebob.geta.databinding.ActivityMainBinding
 import com.kebob.geta.ui.CustomActionBar
 import com.kebob.geta.ui.UserRegisterActivity
+import com.kebob.geta.ui.timelist.DeleteTimeDialogFragment
 import com.kebob.geta.ui.timelist.TimeListActivity
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CheckMealDialogFragment.CheckMealDialogListener {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
@@ -38,7 +41,9 @@ class MainActivity : AppCompatActivity() {
     private val databaseReference = database.reference
 
     private lateinit var sharedPreferences: SharedPreferences
-    private lateinit var userName : String
+    private lateinit var userName: String
+
+    private var position: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,7 +66,7 @@ class MainActivity : AppCompatActivity() {
         })
 
         sharedPreferences = getSharedPreferences(UserRegisterActivity.login, MODE_PRIVATE)
-        userName = sharedPreferences.getString(UserRegisterActivity.userN,"default").toString()
+        userName = sharedPreferences.getString(UserRegisterActivity.userN, "default").toString()
 
         mealList = intent.getSerializableExtra("meals") as MutableList<Meal>
 
@@ -84,8 +89,7 @@ class MainActivity : AppCompatActivity() {
             override fun onItemClick(view: View, position: Int) {
                 if (position < mealList.size - 1 && mealList[position + 1].person != "") return
                 if (position > 0 && mealList[position - 1].person == "") return
-                updateData(position)
-                updateMealList()
+                showConfirmDialog(position)
             }
         })
     }
@@ -172,5 +176,19 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun showConfirmDialog(pos: Int) {
+        position = pos
+        val dialog = CheckMealDialogFragment()
+        dialog.show(supportFragmentManager, "CheckMealDialogFragment")
+    }
+
+    override fun onDialogPositiveClick(dialog: DialogFragment) {
+        updateData(position)
+        updateMealList()
+    }
+
+    override fun onDialogNegativeClick(dialog: DialogFragment) {
     }
 }
